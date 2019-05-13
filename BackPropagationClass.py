a4806ea18e6ae3c96bc67706c6bc60d950d4be8b
Julio Adan Montano <juliomontano008@gmail.com>
2019-05-12 18:27:49 -0600

    Comienzo de la función entrenar

--- BackPropagationClass.py
+++ BackPropagationClass.py
@@ -1,16 +1,25 @@
-class Backpropagation:
+import random
 
+class Backpropagation:
     def __init__(self, dataSet, expectedOutputs, n_hiddenNodes, n_outputNodes):
         self.dataSet = dataSet
         self.expectedOutputs = expectedOutputs
         self.n_hiddenNodes = n_hiddenNodes
         self.n_outputNodes = n_outputNodes
-
+        self.a = 0.25
+        self.hidden_weights = []
+        self.outputs_weights = []
+        self.entry_data = []
+        self.hidden_data = []
+        self.output_data = []
+        self.error_obtained = 0
+        
     def multiply_matrices(self, A, B):    
-        if len(A) != len(B):
+        '''if len(A) != len(B):
             print("Las matrices no son compatibles")
-            exit()        
-        result = []
+            exit()        '''
+        
+        result = []                                
         for i in range(0,len(B[0])):
             sumatory = 0
             for j in range(0,len(A)):                        
@@ -28,10 +37,10 @@ class Backpropagation:
 
     def substractMatrix(self, matrixWeight, updatedMatrixWeights): #substract matrix values, they must be the same length.
         resultMatrix = []
-        for i,weight in enumerate(matrix):
+        for i,weight in enumerate(matrixWeight):
             values = []
             for j,value in enumerate(weight):
-                values.append( matrixWeight[i][j]-matrixUpdated[i][j] )        
+                values.append( matrixWeight[i][j]-updatedMatrixWeights[i][j] )        
             resultMatrix.append( values )
         print(resultMatrix)
 
@@ -39,4 +48,41 @@ class Backpropagation:
         return predict - actual
 
     def error(self, predict, actual):
-        1/2*( (predict-actual)**2 )
+        return 1/2*( (predict-actual)**2 )
+
+    def generate_random_matrix(self, rows, colums):
+        matrix = []
+        for i in range(0, rows):
+            row = []
+            for j in range(0, colums):
+                row.append(random.random())
+            matrix.append(row)
+        return matrix 
+    
+    def generate_array(self, elements):
+        new_array = []
+        for i in range(0, elements):
+            new_array.append(0)
+        return new_array
+    
+    def train(self):
+        self.hidden_weights = [[0.11, 0.12], [0.21, 0.08]]#self.generate_random_matrix(len(self.dataSet[0]), self.n_hiddenNodes)
+        self.outputs_weights = [[0.14],[0.15]]#self.generate_random_matrix(self.n_hiddenNodes, self.n_outputNodes)                
+        
+        self.hidden_data = self.generate_array(self.n_hiddenNodes)
+        self.output_data = self.generate_array(self.n_outputNodes)
+
+        for index, value in enumerate(self.dataSet):            
+            self.hidden_data = self.multiply_matrices(value, self.hidden_weights)
+            self.output_data = self.multiply_matrices(self.hidden_data, self.outputs_weights)
+            self.error_obtained =  self.error(self.output_data[0], self.expectedOutputs[index])                        
+            print(self.error_obtained)
+
+x = Backpropagation([[2,3]], [1],2, 1)
+x.train()
+        
+
+    
+
+
+    
--- backPropagation.py
+++ backPropagation.py
@@ -1,45 +1,5 @@
 
 import Backpropagation from BackPropagationClass
 
-def multiply_matrices(A, B):    
-    if len(A) != len(B):
-        print("Las matrices no son compatibles")
-        exit()        
-    result = []
-    for i in range(0,len(B[0])):
-        sumatory = 0
-        for j in range(0,len(A)):                        
-            sumatory += A[j]*B[j][i]
-        result.append(sumatory)
-    return result
-
-
-def multiply_matrix_number(scalar, matrix):
-    rows = len(matrix)
-    columns = len(matrix[0])    
-    for i in range(0,rows ):
-        for j in range(0, columns):
-            matrix[i][j] = matrix[i][j]*scalar
-    return matrix
-
-
-
-def substractMatrix(matrixWeight, updatedMatrixWeights): #substract matrix values, they must be the same length.
-    resultMatrix = []
-    for i,weight in enumerate(matrix):
-        values = []
-        for j,value in enumerate(weight):
-            values.append( matrixWeight[i][j]-updatedMatrixWeights[i][j] )        
-        resultMatrix.append( values )
-    print(resultMatrix)
-
-
-def delta(predict, actual): #actual is our expected output
-    return predict - actual
-
-def error(predict, actual):
-    1/2*(predict-actual)**2
-
-
 def main():
     init = Propagation():
